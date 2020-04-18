ae9fe11b37f1f7201574dc74a506074d11029cd9
Sascha Gogolin <saschagogolin@gmail.com>
2020-04-18 20:22:42 +0000

    Added first test of product models

--- products/models.py
+++ products/models.py
@@ -23,5 +23,5 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     image = models.ImageField(upload_to="images")
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
