8b4bf1dbeabe47d924e27294fcb4b5bbca988780
Sascha Gogolin <saschagogolin@gmail.com>
2020-04-19 14:54:23 +0000

    Added urls for create product and product detail pages

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from .views import all_products, products_animal_cat, products_animal_dog, products_category_clothes, products_category_toys, products_category_supplies
+from .views import all_products, products_animal_cat, products_animal_dog, products_category_clothes, products_category_toys, products_category_supplies, product_detail, create_product
 
 urlpatterns = [
     url(r'^all_products/$', all_products, name="all_products"),
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^all_products/clothes/$', products_category_clothes, name="products_category_clothes"),
     url(r'^all_products/toys/$', products_category_toys, name="products_category_toys"),
     url(r'^all_products/supplies/$', products_category_supplies, name="products_category_supplies"),
+    url(r'^(?P<id>\d+)/$', product_detail, name='product_detail'),
+    url(r'^donate/$', create_product, name='create_product'),
 ]
