a6a8548bec50412527b7dae54d2867f77eb6391d
Sascha Gogolin <saschagogolin@gmail.com>
2020-04-22 10:14:46 +0000

    Minor bug fixes

--- cart/views.py
+++ cart/views.py
@@ -4,7 +4,7 @@ def view_cart(request):
     """A view that renders the cart contents page"""
     return render(request, "cart.html")
 
-def add_to_cart(request, id):
+def add_item_to_cart(request, id):
     """Add a quantity of the specified product to the cart"""
     quantity=int(request.POST.get('quantity'))
 
@@ -16,7 +16,7 @@ def add_to_cart(request, id):
         cart[id] = cart.get(id, quantity) 
 
     request.session['cart'] = cart
-    return redirect(reverse('view_cart'))
+    return redirect(reverse('index'))
 
 def adjust_cart(request, id):
     """Adjust the quantity of the specified product to the specified amount"""
