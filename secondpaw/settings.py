b251763a1f77aa60bdf50a16585fe8adb5f3c074
Sascha Gogolin <saschagogolin@gmail.com>
2020-05-08 09:30:22 +0000

    Removed MEDIAFILES_LOCATION & DEFAULT_FILE_STORAGE

--- secondpaw/settings.py
+++ secondpaw/settings.py
@@ -142,12 +142,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIAFILES_LOCATION = 'media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
