b74d087eff6ae890f4a0c0bbcb96016e0e98d8da
Sascha Gogolin <saschagogolin@gmail.com>
2020-05-08 09:12:17 +0000

    Changed MEDIA_ROOT to 'mediafiles'

--- secondpaw/settings.py
+++ secondpaw/settings.py
@@ -145,7 +145,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
