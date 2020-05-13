8cf7ae95b131a25850c87c5c7a2d766c903073c1
Sascha Gogolin <saschagogolin@gmail.com>
2020-05-13 19:22:41 +0000

    Added a conditional for database usage

--- secondpaw/settings.py
+++ secondpaw/settings.py
@@ -88,14 +88,19 @@ WSGI_APPLICATION = 'secondpaw.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
-DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+if DEBUG:
+    # DEVELOPEMENT DATABASE
+    print("TEST DATABASE RUNNING")
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+else:
+    # PRODUCTION DATABASE
+    print("PRODUCTION DATABASE RUNNING")
+    DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 # Password validation
