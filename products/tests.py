df57aa895021074a5261c968dd38371215892eae
Sascha Gogolin <saschagogolin@gmail.com>
2020-05-14 11:32:53 +0000

    Changed name to title on product test

--- products/tests.py
+++ products/tests.py
@@ -5,5 +5,5 @@ class ProductTests(TestCase):
     """Tests to be run against the Product models"""
 
     def test_str(self):
-        test_name = Product(name='A product')
-        self.assertEqual(str(test_name), 'A product')
+        test_title = Product(title='A product')
+        self.assertEqual(str(test_title), 'A product')
