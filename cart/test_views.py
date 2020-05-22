d651f9739619f772ca7fa764a2d22b8571c531d8
Sascha Gogolin <saschagogolin@gmail.com>
2020-05-22 17:42:52 +0000

    Fixed PEP8 validation errors

--- cart/test_views.py
+++ cart/test_views.py
@@ -1,8 +1,9 @@
 from django.test import TestCase, Client
 from django.shortcuts import get_object_or_404
 
+
 class TestViews(TestCase):
-    
+
     def test_get_view_cart_page(self):
         page = self.client.get("/cart/")
         self.assertEqual(page.status_code, 200)
--- cart/views.py
+++ cart/views.py
@@ -1,27 +1,43 @@
 from django.shortcuts import render, redirect, reverse
 import sweetify
 
+
 def view_cart(request):
-    """A view that renders the cart contents page"""
+    """
+    A view that renders the cart contents page
+    """
     return render(request, "cart.html")
 
+
 def add_item_to_cart(request, id):
-    """Add a quantity of the specified product to the cart"""
-    quantity=int(request.POST.get('quantity'))
+    """
+    Add a quantity of the specified product to the cart
+    """
+    quantity = int(request.POST.get('quantity'))
 
     cart = request.session.get('cart', {})
-    """If statement that ensures a customer can't add/create duplicates of the same product to the shopping cart"""
+    """
+    If statement that ensures a customer can't add/create
+    duplicates of the same product to the shopping cart
+    """
     if id in cart:
-        cart[id] = int(cart[id]) + quantity      
+        cart[id] = int(cart[id]) + quantity
     else:
-        cart[id] = cart.get(id, quantity) 
+        cart[id] = cart.get(id, quantity)
 
     request.session['cart'] = cart
-    sweetify.success(request, "Your selection has been added to your shopping cart!", icon="success")
+    sweetify.success(request,
+                     """Your selection has been added
+                     to your shopping cart!""",
+                     icon="success")
     return redirect(reverse('view_cart'))
 
+
 def adjust_cart(request, id):
-    """Adjust the quantity of the specified product to the specified amount"""
+    """
+    Adjust the quantity of the specified
+    product to the specified amount
+    """
     quantity = int(request.POST.get('quantity'))
 
     cart = request.session.get('cart', {})
@@ -32,5 +48,8 @@ def adjust_cart(request, id):
         cart.pop(id)
 
     request.session['cart'] = cart
-    sweetify.success(request, "Successfully removed from your shopping cart!", icon="success")
+    sweetify.success(request,
+                     """Successfully removed from
+                     your shopping cart!""",
+                     icon="success")
     return redirect(reverse('view_cart'))
