3b9b589faf103de8fccf31431074349c92e4abd4
Sascha Gogolin <saschagogolin@gmail.com>
2020-05-23 16:29:51 +0000

    Added max_length to subject and message fields

--- home/forms.py
+++ home/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class ContactForm(forms.Form):
     from_email = forms.EmailField(required=True)
-    subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    subject = forms.CharField(max_length=50, required=True)
+    message = forms.CharField(max_length=500, widget=forms.Textarea, required=True)
