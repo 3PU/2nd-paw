5e78d54b110a1a82056bec1b522af8400988bc8b
Sascha Gogolin <saschagogolin@gmail.com>
2020-05-24 11:57:10 +0000

    Fixed minor code validation errors

--- checkout/test_urls.py
+++ checkout/test_urls.py
@@ -2,8 +2,9 @@ from django.test import TestCase
 from django.urls import reverse, resolve
 from checkout.views import checkout
 
+
 class TestUrls(TestCase):
-    
-    def test_checkout_url_is_resolved(self):
-        url = reverse("checkout")
-        self.assertEquals(resolve(url).func, checkout)
+
+        def test_checkout_url_is_resolved(self):
+            url = reverse("checkout")
+            self.assertEquals(resolve(url).func, checkout)
