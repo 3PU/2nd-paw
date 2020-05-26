5b2d2cea6fcd5c577e0c1c008962efdd5395f655
Sascha Gogolin <saschagogolin@gmail.com>
2020-05-26 10:46:40 +0000

    Commented out conditional statement for databases

--- secondpaw/settings.py
+++ secondpaw/settings.py
@@ -91,17 +91,17 @@ WSGI_APPLICATION = 'secondpaw.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-if "DATABASE_URL" in os.environ:
-    print("Database found. Using PostgresSQL.")
-    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-else:
-    print("Database URL not found. Using SQLite instead.")
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#if "DATABASE_URL" in os.environ:
+#    print("Database found. Using PostgresSQL.")
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+#else:
+#    print("Database URL not found. Using SQLite instead.")
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
